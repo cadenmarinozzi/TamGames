2979aaeae49f1e0948d067e96a7d2156992bcc9e
nekumelon <nekumelon@Cadens-MacBook-Pro.local>
2023-01-26 12:58:21 -0800

    Superhot

---
+++ src/Games/Superhot/Superhot.js
@@ -0,0 +1,13 @@
+import Game from 'Components/containers/Game';
+import './Superhot.scss';
+
+function Superhot() {
+	return (
+		<Game
+			title='Superhot'
+			url='https://tamgames.nekumelon.repl.co/Games/Superhot/superhot.html'
+		/>
+	);
+}
+
+export default Superhot;
